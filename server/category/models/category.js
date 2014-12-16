bb4ae8a518bd9946dcce63c644fc6d1a73d98386
jsmit032 <jsmit032@ucr.edu>
2014-12-16 12:33:26 -0800

    added category to database

---
+++ server/category/models/category.js
@@ -0,0 +1,10 @@
+var mongoose = require('mongoose'),
+	Schema = mongoose.Schema;
+
+var CategorySchema = new Schema({
+
+	name: String
+
+});
+
+module.exports = mongoose.model('Category', CategorySchema);
