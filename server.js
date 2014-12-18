0d362dfe6676b49ba08bfb11661a606da2feae94
stephjin <stephanie.k.jin@gmail.com>
2014-12-17 16:26:38 -0800

    Restructure files and use ui router.

--- server.js
+++ server.js
@@ -1,21 +1,44 @@
-var express		= require('express'),
-	app			= express(),
-	bodyParser	= require('body-parser'),
-	mongoose	= require('mongoose'),
-	// path = require('path'),
-	database = require('./config/database');
+var express   = require('express'),
+  app     = express(),
+  port = process.env.PORT || 3000, 
+  path = require('path'),
+  bodyParser  = require('body-parser'),
+  mongoose  = require('mongoose'),
+  database = require('./config/database'),
+  WunderNodeClient = require('wundernode'),
+  URL = require('url');
 
 mongoose.connect(database.url);
 
-app.use(express.static(__dirname + '/client'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+app.use(express.static(__dirname + '/public'));
+
+app.set('view engine', 'ejs');
+
+
+
+// wunderground api
+var apikey = "2f0b44146ceab5a4";
+var zip = 10021;
+var debug = false;
+var wunder = new WunderNodeClient(apikey, debug,  10, 'minute');
+
+app.get('/conditions/forecast', function(req, res){
+   var query = zip;
+   console.log('query: ' +  query);
+   console.log('URL: ' + URL);
+    wunder.conditionsforecast(query, function(err, obj) {
+      if (err){
+        console.log('errors: ' + err);
+        res.end("Error processing query string:" + queryData.query);
+      }
+      res.end(obj);
+    });
+});
+
 require('./server/routes.js')(app);
-
-
-// the port will be whatever we set it to or default to 3000;
-var port        = process.env.PORT || 3000;
 
 app.listen(port);
 
