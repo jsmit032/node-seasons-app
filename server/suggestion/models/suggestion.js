141b5708611c29f0df8bcfce0ea7c480f15d969a
jsmit032 <jsmit032@ucr.edu>
2014-12-17 21:11:41 -0800

    added suggestion folder for many-to-many relationship building

---
+++ server/suggestion/models/suggestion.js
@@ -0,0 +1,14 @@
+suggestion.js
+
+// Example Many to many schema below from:
+// http://stackoverflow.com/questions/11117854/many-to-many-mapping-with-mongoose
+// var Schema = mongoose.Schema,
+//     ObjectId = Schema.ObjectId;
+//     PackageSchema = require('./path/to/package-schema.js')
+
+// var FlashcardSchema = new Schema({
+//     id      : ObjectId,
+//     type        : { type: String, default: '' },
+//     story       : { type: String, default: '' },
+//     packages    : [ PackageSchema ]
+// });
