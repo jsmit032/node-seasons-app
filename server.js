ee92fcee1802343151d1703c5573b341f552e8c5
jsmit032 <jsmit032@ucr.edu>
2014-12-17 19:35:25 -0800

    modified package.json and connected heroku mongoDB

--- server.js
+++ server.js
@@ -2,10 +2,22 @@ var express		= require('express'),
 	app			= express(),
 	bodyParser	= require('body-parser'),
 	mongoose	= require('mongoose'),
+	mongodbUri = require('mongodb-uri'),
 	// path = require('path'),
 	database = require('./config/database');
 
-mongoose.connect(database.url);
+var uri = 'mongodb://heroku_app32583861:f02opcjc007nor4rg1c1otts07@ds027751.mongolab.com:27751/heroku_app32583861';
+
+var mongooseConnectString = mongodbUri.formatMongoose(uri);
+mongoose.connect(mongooseConnectString);
+// mongoose.connect(database.url);
+
+// Test for connection success
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'Connection error: '));
+db.once('open', function callback () {
+    console.log('Successfully connected to MongoDB');
+})
 
 app.use(express.static(__dirname + '/client'));
 app.use(bodyParser.urlencoded({ extended: true }));
